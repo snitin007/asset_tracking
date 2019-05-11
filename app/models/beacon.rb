70f3e82e75f53ba6ba038dcebd397dc196e4dccc
Ritwick <malhotraritwick2011@gmail.com>
2019-05-11 20:59:53 +0200

    can't take blank validatino

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -1,3 +1,7 @@
 class Beacon < ApplicationRecord
   belongs_to :central
+  validates :orientation, presence: true
+  validates :distance, presence: true
+  validates :sensor_id, presence: true
+
 end
