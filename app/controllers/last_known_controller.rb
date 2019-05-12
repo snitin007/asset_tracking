5d675fbf67a4d7ab4056d1721cbc9c93e160865a
Ritwick <malhotraritwick2011@gmail.com>
2019-05-12 07:56:37 +0200

    sa

--- app/controllers/last_known_controller.rb
+++ app/controllers/last_known_controller.rb
@@ -1,3 +1,10 @@
-class last_known_controller < ApplicationController 
-def index; end
+class LastKnownController < ApplicationController 
+	def index
+		@sensors = Beacon.all
+		@locations = Array.new
+		unique_ids = @sensors.pluck(:sensor_id).uniq
+		unique_ids.each do |id|
+			@locations.push(Beacon.find_by(sensor_id: id))
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'centrals#index'
   get 'show_all', to: 'main_display#index'
+  get 'sensors', to: 'last_known#index'
   devise_for :users
   resources :centrals do
   	resources :beacons
